2f02322bd10c80b880036dcd58719f74636ac9b4
Mirgubad <migubad@code.edu.az>
2022-06-11 03:08:16 +0400

    Add project files.

---
+++ ConsoleApp12/MyStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class MyStack
+    {
+        private int maxsize { get; set; } = 1000;
+        private int _index;
+        private object[] arr;
+
+        public MyStack()
+        {
+            arr = new object[maxsize];
+            _index = -1;
+        }
+        public bool IsEmpty()
+        {
+            return _index < 0;
+        }
+
+        public object Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("There is no element in stack");
+            }
+
+            object value = arr[_index];
+            arr[_index] = null;
+            _index--;
+            return value;
+
+        }
+        public object Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("There is no element in stack");
+
+            }
+            return arr[_index];
+        }
+        public void Push(object value)
+        {
+            if (_index >= maxsize - 1)
+            {
+                throw new Exception("Stac is Full");
+            }
+            else
+            {
+                _index++;
+                arr[_index] = value;
+            }
+        }
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                if (item != null)
+                    Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace ConsoleApp12
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            MyStack  names = new MyStack();
+            names.Push("Salman");
+            names.Push(23);
+            names.Push(3);
+            Console.WriteLine(names.Pop());
+            Console.WriteLine(names.Peek());
+
+
+
+           
+        }
+
+        
+    }
+}
