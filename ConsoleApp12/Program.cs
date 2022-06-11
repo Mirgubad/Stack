8367c264f4414fb5738e2cbc9096c7e8af74c4b6
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-06-11 12:34:24 +0400

    Update Program.cs

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -8,14 +8,19 @@ namespace ConsoleApp12
         static void Main(string[] args)
         {
             MyStack  names = new MyStack();
-            names.Push("Salman");
+            names.Push("John");
             names.Push(23);
             names.Push(3);
             Console.WriteLine(names.Pop());
             Console.WriteLine(names.Peek());
 
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
 
-
+//Objetc arrayi olduğu üçün maində loop emtək olmur,IEnumerable -a implement etdimki maində loop edib object arrayinin İtemlarını çap etmək olsun
+//Həmçinin o olmasa belə GetAll metodu yazmışamki onu call edəndə print edir elementləri
            
         }
 
